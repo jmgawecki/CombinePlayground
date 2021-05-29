518590cb578efd2d7e3dc85f755a31a4b4aef4a9
Kuba <jmgawecki@icloud.com>
2021-05-29 11:02:21 +0100

    Initial Commit

---
+++ CombinePlayground/CombinePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinePlaygroundApp.swift
+//  CombinePlayground
+//
+//  Created by Jakub Gawecki on 29/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct CombinePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
